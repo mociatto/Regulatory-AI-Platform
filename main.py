5233bcd87e839c3101449c1a0297d348753406a0
Mostafa Anoosha <mosi-macbook@Mostafas-MacBook-Air-1242.local>
2025-07-20 23:45:27 +0100

    connecting to cloud

--- main.py
+++ main.py
@@ -205,3 +205,5 @@ if __name__ == "__main__":
     # Cloud Run will use a production WSGI server instead
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port) 
+
+# cloud test
